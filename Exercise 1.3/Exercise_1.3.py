56c12b1e5379f6ce68cd48c5f73ab0e5ec68648a
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-08-09 11:04:44 -0400

    Add files via upload

---
+++ Exercise 1.3/Exercise_1.3.py
@@ -0,0 +1,51 @@
+recipes_list = []
+ingredients_list = []
+
+def take_recipe():
+    name_input = input("Recipe name: ")
+    cooking_time_input = int(input("Cooking time: "))
+    ingredients_input = input("Enter ingredients seperated by commas: ")
+    ingredients = [ingredient.strip() for ingredient in ingredients_input.split(",")]
+
+    recipe = {
+        'name': name_input,
+        'cooking_time': cooking_time_input,
+        'ingredients': ingredients,
+    }
+    return recipe
+
+n = int(input("How many recipes would you like to enter? "))
+for i in range(0, n):
+    recipe = take_recipe()
+    for ingredient in recipe['ingredients']:
+        if ingredient in ingredients_list:
+            continue
+        else:
+            ingredients_list.append(ingredient)
+    recipes_list.append(recipe)
+
+for recipe in recipes_list:
+    
+    if recipe['cooking_time'] < 10:
+        if len(recipe['ingredients']) < 4:
+            difficulty = 'Easy'
+        else:
+            difficulty = 'Medium'
+    else:
+        if len(recipe['ingredients'])< 4:
+            difficulty = 'Intermediate'
+        else:
+            difficulty = 'Hard'
+    recipe["difficulty"] = difficulty
+
+    print(f"Recipe: {recipe['name']}")
+    print(f"Cooking Time (min): {recipe['cooking_time']}")
+    print("Ingredients:")
+    for ingredient in recipe['ingredients']:
+        print(ingredient)
+    print(f"Difficulty level: {recipe['difficulty']}")
+
+print("Ingredients Available Across All Recipes")
+print("----------------------------------------")
+for ingredient in ingredients_list:
+    print(ingredient)
